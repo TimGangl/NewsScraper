649afccc25660484244ea0007ee5b2ec750acdf4
Tim Gangl <tim.gangl30@gmail.com>
2020-02-23 17:02:40 -0800

    scraping to database and rendering on page

--- server.js
+++ server.js
@@ -1,52 +1,95 @@
-//dependencies
 const express = require("express");
-const mongoose = require("mongoose");
-const exphbs = require("express-handlebars")
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
-//Port set up
-const PORT = process.env.PORT || 3000;
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-//express and router
+const db = require("./models");
+
+const PORT = 3000;
+
 const app = express();
-const router = express.Router();
 
-require("./routes/routes")(router);
-require("./scripts/scrape");
-//conncetion for handlebars to express
-app.engine("handlebars", exphbs({
-  defaultLayout: "main"
-}));
-app.set("view engine", "handlebars")
-
-//public folder static directory
 app.use(logger("dev"));
-app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(router);
+
+app.use(express.static("public"));
+
+// Set Handlebars
+const Handlebars = require('handlebars')
+const hbs = require("express-handlebars");
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+
+app.engine("handlebars", hbs({ defaultLayout: "main", handlebars: allowInsecurePrototypeAccess(Handlebars) }));
+app.set("view engine", "handlebars");
+
 
 // Connect to Mongo DB
 mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true }, function () {
   console.log("mongo connected")
 });
 
-//sets up for deployment or local host
-var db = process.env.MONGODB_URI || "mongodb://localhost/mainheadlines";
-
-//mongoose connection to databse
-mongoose.connect(db, function (error) {
-  if (error) {
-    console.log(error);
-  }
-  else {
-    console.log("Mongoose Connection Successful!");
-  }
+//Routes
+app.get("/", (req, res) => {
+  db.Article.find({}, function (err, found) {
+    if (err) {
+      console.log(err);
+    }
+    res.render("index", { found });
+  });
 });
 
-//listen on port
+app.get("/saved", (req, res) => {
+  db.Article.find({}, function (err, found) {
+    if (err) {
+      console.log(err);
+    }
+    res.render("saved");
+  })
+});
+
+
+//GET route for scraping the jazztimes site
+app.get("/scrape", function (req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://npr.org/sections/music-news").then(function (response) {
+    var $ = cheerio.load(response.data);
+
+    $("article.item").each(function (i, element) {
+      const result = {};
+
+      result.title = $(this).find("h2.title").text().trim();
+      result.summary = $(this).find("p.teaser").text().trim();
+      result.link = $(this).find("a").attr("href");
+
+
+      // console.log(result);
+
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result).then(function (dbArticle) {
+        // View the added result in the console
+        console.log(dbArticle);
+      })
+        .catch(function (err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+
+    });
+
+  });
+});
+
+//GET route 
+
+
+
+
+
+//start server
 app.listen(PORT, function () {
-  console.log("Listening on Port: " + PORT + "!");
+  console.log("App running on port " + PORT + "!");
 });
